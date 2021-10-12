ac67079008935efdab4d9e046d51122511f65e89
cyberbobby <kavchakvova@gmail.com>
2021-10-12 10:41:41 +0300

    Moved code from template folder to React App src folder; created data.js file for product information; moved images from template folder to public folder and added new images to this images folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,222 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">
+            Online Store
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div className="row centre">
+          <div className="card">
+            <a href="product.html">
+              <img className="mediu" src="./images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Star Wars Master Yoda Mug</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+              </div>
+              <div className="price">$12</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="mediu" src="./images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Star Wars Master Yoda Mug</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+              </div>
+              <div className="price">$12</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="mediu" src="./images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Star Wars Master Yoda Mug</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+              </div>
+              <div className="price">$12</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="mediu" src="./images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Star Wars Master Yoda Mug</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+              </div>
+              <div className="price">$12</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="mediu" src="./images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Star Wars Master Yoda Mug</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+              </div>
+              <div className="price">$12</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="mediu" src="./images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Star Wars Master Yoda Mug</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star-half-o"></i>{' '}
+                </span>
+                <span>
+                  {' '}
+                  <i className="fa fa-star-o"></i>{' '}
+                </span>
+              </div>
+              <div className="price">$12</div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
