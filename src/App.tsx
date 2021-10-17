47d13839c861ddd7f9e7be8c3f6e024ff372f337
cyberbobby <kavchakvova@gmail.com>
2021-10-17 23:01:00 +0300

    Implemented add to cart action:
    - In App.tsx was changed "a" to Link in order not to refresh the page when user click on a link. Also next to the cart was add "badge";
    - In store.js was added a new reducer for cart;
    - In cartActions was added new  action addToCart and possibility to save item that was added to cart to localStorage;
    - In CartScreen.js  was ued addToCart action  when there is a product to add to the cart;
    - In App.tsx was found and error in line 9 "const cart = useSelector((state) => state.cart);". Error description: "Property 'cart' does not exist on type 'DefaultRootState'.  TS2339". To avoid this error temporary add "any" to "state".

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
 
 function App() {
+  const cart = useSelector((state: any) => state.cart); // receive error here "Property 'cart' does not exist on type 'DefaultRootState'.  TS2339", temporary add "any" to "state"
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               Online Store
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
